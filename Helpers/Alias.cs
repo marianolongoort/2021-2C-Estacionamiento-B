852ee9e85140df83c20d860f5fc8e6a964518525
marianolongoort <mariano.longo@ort.edu.ar>
2021-10-20 20:49:18 -0300

    Codigo Ejemplo. Solicitado NO COPY PASTE

---
+++ Helpers/Alias.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstacionamientoMVC.Helpers
+{
+    public static class Alias
+    {
+        public const string DNI = "Documento";
+        public const string Email = "Correo electrónico";
+        public const string ClienteId = "Cliente";
+        public const string PersonaId = "Persona";
+        public const string Password = "Contraseña";
+        public const string PassConfirm = "Confirmación de contraseña";
+        public const string EmpleadoId = "Empleado";
+        public const string VehiculoId = "Vehiculo";
+        public const string Anio = "Año";
+        public const string NombreCompleto = "Nombre completo";
+        public const string RolName = "Nombre";
+    }
+}
